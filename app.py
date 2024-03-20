f0a3e6290c18c086b688eea250a42b1b10f313c3
se4w33d <55843883+se4w33d@users.noreply.github.com>
2024-03-21 00:07:22 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request
+import requests
+import json
+import time
+
+TRAINEES = [
+    "CHANEL KOH XUE MEI",
+    "CHRISTINE MARLINA",
+    "TAN KOK HOW",
+    "CHAW FOONG LEAN",
+    "VENKATESH SIDDENALLI OBANAIK",
+    "CHONG WEI SHENG",
+    "KWA CHEONG HONG",
+    "SOON YI XIANG, ANDREW",
+    "ANGELIN TANIUS",
+    "KELLY KWEK FU LIANG",
+    "YAO SIYUE",
+    "NIDHI ATUL MISTRY",
+    "CHAI SHU YING",
+    "ALISON WONG CHIONG CHING",
+    "GAN BENG HUAT ALVIN",
+    "LIM WEIKANG, DERIC",
+    "NUR ATIQAH BINTE MOHAMED AZMAN",
+    "CHUA SOK CHING",
+    "TAY KIT HUAT",
+    "ONG KOK GUAN",
+    "ANG YILIN",
+    "CARLA TJANDRA",
+    "PNG SEOW HUI",
+    "LEE XUAN YING, SARA",
+    "KWEE LANNY KURNIANINGRUM",
+    "LEE SI ZHENG",
+    "LI GUANGYIN",
+    "KONG WAI CHONG",
+    "LAW TE HAO",
+    "KOH KHIA HUI, REGINA",
+    "LOK SIEW WEI",
+    "MUHAMED FIRDAUS BIN MOHAMED JAINI",
+    "JAVEN LIM JIA HAO",
+    "TAN NGAM TECK",
+    "SYLVESTER NG AIK TONG",
+    "JOCELYN HO KANGLI",
+]
+
+NUMBER_OF_TRAINEES = len(TRAINEES)
+
+parameters = {
+    "attendanceCode": "BV68743",
+    "motCode": "41"
+}
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+@app.route("/index", methods=["GET"])
+def index():
+    response = requests.get(
+        "https://www.myskillsfuture.gov.sg/api/get-attendance", params=parameters
+    )
+
+    res = response.json()
+
+    # print(json.dumps(res, sort_keys=True, indent=4))
+    # print(response.status_code)
+
+    # extract the trainee's name from the response
+    trainee_attendance = [trainee['name'] for trainee in res]
+    
+
+    # signed_trainees = []
+    # not_signed_trainees = []
+
+    # for trainee in TRAINEES:
+    #     if trainee in trainee_attendance:
+    #         signed_trainees.append(trainee)
+    #     else:
+    #         not_signed_trainees.append(trainee)
+   
+
+    trainees_status = {}
+    for trainee in TRAINEES:
+        if trainee in trainee_attendance:
+            trainees_status[trainee] = 'signed'
+        else:
+            trainees_status[trainee] = 'not signed'
+
+    res1 = dict(list(trainees_status.items())[:len(trainees_status)//2]) 
+    res2 = dict(list(trainees_status.items())[len(trainees_status)//2:])
+
+    print(trainees_status)
+    return render_template("index.html", status1=res1, status2=res2)
+
+
+
+# print(signed_trainees)
+# print(not_signed_trainees)
+
